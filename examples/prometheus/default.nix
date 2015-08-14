089eaad610a4e1fd1ca0426c56126c8ded0fdb01
Rafik Salama <r@rafiksalama.com>
2015-08-14 13:08:53 -0400

    Add prometheus example

---
+++ examples/prometheus/default.nix
@@ -0,0 +1,39 @@
+{ nixpkgs ? <nixpkgs> }:
+
+let
+
+  pkgs = import nixpkgs {};
+
+  aurora = import ../../lib/aurora.nix { inherit pkgs; };
+
+  files = pkgs.stdenv.mkDerivation rec {
+    name = "files";
+    src = ./files;
+    buildCommand = "mkdir -p $out && cp ${src}/* $out/";
+  };
+
+  prometheusProcess = aurora.Process {
+    name = "prometheus";
+    cmdline = ''
+      ${pkgs.prometheus}/bin/prometheus \
+        -config.file ${files}/prometheus.yml \
+        -web.listen-address 0.0.0.0:{{thermos.ports[http]}} \
+        -log.level=debug
+      '';
+  };
+
+  prometheusTask = aurora.Task {
+    processes = [ prometheusProcess ];
+    resources = {
+      cpu = 1;
+      ram = 8 * aurora.utils.MB;
+      disk = 1 * aurora.utils.GB;
+    };
+  };
+
+in aurora.Service {
+  cluster = "devcluster";
+  environment = "devel";
+  role = "vagrant";
+  task = prometheusTask;
+}
